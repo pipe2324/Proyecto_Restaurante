9ad37355c48f2c7ecd2a934f211f1ba4c95f4b51
andres felipe <andreshurtado242@gmail.com>
2023-04-10 15:20:47 -0500

    Redireccionado del combobox a los diferentes formularios.

--- Proyecto_Restaurante/Form2.cs
+++ Proyecto_Restaurante/Form2.cs
@@ -43,11 +43,30 @@ namespace Proyecto_Restaurante
 
         private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
         {
-            this.Hide();
-            Form Form2 = new Form3();
-            Form2.ShowDialog();
-
-           
+            
+            
+            if (comboBox1.Text == "ACCESO")
+            {
+                this.Hide();
+                Form Form1 = new Form1();
+                Form1.ShowDialog();
+            }
+            else if(comboBox1.Text== "CLIENTES")
+            {
+                this.Hide();
+                Form Form3 = new Form3();
+                Form3.ShowDialog();
+            }else if (comboBox1.Text == "PAGOS")
+            {
+                this.Hide();
+                Form Form4 = new Form4();
+                Form4.ShowDialog();
+            }else if(comboBox1.Text== "FACTURA")
+            {
+                this.Hide();
+                Form Form5= new Form5();
+                Form5.ShowDialog();
+            }
         }
     }
 }
